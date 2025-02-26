a1fb9c42a5a75f631381ed25fe983c61d5e5a88d
Vishakha Motwani <vishakha@Mohinishs-MacBook-Pro.local>
2025-02-25 18:47:28 -0800

    test project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+const Pizza = (props) => {
+  return React.createElement("div", null, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.ingredients),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, { name: "Hawaiian", ingredients: "Pineapple" }),
+    React.createElement(Pizza, {
+      name: "Cheese",
+      ingredients: "Mozzarella cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "Tandoori chicken",
+      ingredients: "Chicken",
+    }),
+    React.createElement(Pizza, {
+      name: "Veggie Delight",
+      ingredients: "Veggies",
+    }),
+    React.createElement(Pizza, {
+      name: "BBQ chicken",
+      ingredients: "Bbq sauce",
+    }),
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
