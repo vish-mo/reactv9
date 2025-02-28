84af91684282345b718f107fe404cc270e7ecc71
vish-mo <github@vish.sh>
2025-02-27 22:14:29 -0800

    jsx

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -11,7 +11,7 @@ const Pizza = (props) => {
   return (
     <div className="pizza">
       <h1>{props.name}</h1>
-      <p>{props.description}</p>
+      <p>{props.ingredients}</p>
     </div>
   );
 };
